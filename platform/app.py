2a3c91695f2199d5484d61ef9870531249396ba6
Yiwei Lou <48986499+cyfqylyw@users.noreply.github.com>
2024-07-28 23:04:12 +0800

    Update app.py

--- platform/app.py
+++ platform/app.py
@@ -199,5 +199,5 @@ def set_invalid():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5321, debug=True)
-    app.run(host="127.0.0.1", port=5321, debug=True)
+    app.run(host='0.0.0.0', port=5321, debug=True)
+    # app.run(host="127.0.0.1", port=5321, debug=True)
